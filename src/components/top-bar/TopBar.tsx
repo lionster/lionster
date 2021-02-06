70b14c89d0a5f6d57279129ab0b77f48504db45c
codemile <nick@reactgular.com>
2021-02-06 15:56:39 -0500

    updated

--- src/components/top-bar/TopBar.tsx
+++ src/components/top-bar/TopBar.tsx
@@ -1,12 +1,21 @@
 import {FunctionComponent} from 'react';
+import {Button, FormControl, InputGroup} from 'react-bootstrap';
 
 export interface TopBarProps {
+    className?: string;
 }
 
-const TopBar: FunctionComponent<TopBarProps> = () => {
+const TopBar: FunctionComponent<TopBarProps> = ({className}) => {
     return (
-        <div className="flex">
-            TopBar
+        <div className={`flex p-4 ${className || ''}`}>
+            <InputGroup>
+                <InputGroup.Prepend>
+                    <InputGroup.Text>@</InputGroup.Text>
+                </InputGroup.Prepend>
+                <FormControl placeholder="Search"/>
+            </InputGroup>
+            <Button className="flex whitespace-nowrap ml-2">Sign Up</Button>
+            <Button className="flex whitespace-nowrap ml-2">Log In</Button>
         </div>
     );
 };
