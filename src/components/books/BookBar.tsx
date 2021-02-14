f58f9fc7f8bfc7f514f6c1200be9402b2520ff3a
codemile <nick@reactgular.com>
2021-02-14 10:22:10 -0500

    updated

--- src/components/books/BookBar.tsx
+++ src/components/books/BookBar.tsx
@@ -1,26 +1,22 @@
 import Link from 'next/link';
-import {FaStar} from 'react-icons/fa';
-import {BookEntity} from '../../entities/book-entity';
-import {environment} from '../../environment/environment';
-import {DivAnd, DivComponent} from '../utils';
+import { FaStar } from 'react-icons/fa';
+import { BookEntity } from '../../entities/book-entity';
+import { environment } from '../../environment/environment';
+import { DivAnd, DivComponent } from '../utils';
 
 export interface BookBarProps {
     book: BookEntity;
 }
 
-export const BookBar: DivComponent<BookBarProps> = ({className, book}) => {
+export const BookBar: DivComponent<BookBarProps> = ({ className, book }) => {
     return (
         <DivAnd className="inline-flex align-middle" and={className}>
             <Link href="/app/dashboard">
-                <a className="btn font-bold">
-                    {environment.brandName}
-                </a>
+                <a className="btn font-bold">{environment.brandName}</a>
             </Link>
+            <button className="btn">{book.title}</button>
             <button className="btn">
-                {book.title}
-            </button>
-            <button className="btn">
-                <FaStar/>
+                <FaStar />
             </button>
         </DivAnd>
     );
