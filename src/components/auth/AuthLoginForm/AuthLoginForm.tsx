a9173de2ace2dc3d54be5b5fb4dbab4ecfff6211
codemile <nick@reactgular.com>
2021-02-21 10:40:38 -0500

    fixes feedback

--- src/components/auth/AuthLoginForm/AuthLoginForm.tsx
+++ src/components/auth/AuthLoginForm/AuthLoginForm.tsx
@@ -12,42 +12,49 @@ export const AuthLoginForm: FunctionComponent = () => {
     const formik = useFormik({
         validationSchema: schema,
         initialValues: {email: '', password: ''},
-        onSubmit: (values) => console.log(values)
+        onSubmit: (values) => console.error(values)
     });
 
     return (
         <Form
-            noValidate
             className="flex flex-col mb-6"
+            noValidate
+            validated={Boolean(formik.submitCount)}
             onSubmit={formik.handleSubmit}
         >
             <Form.Group controlId="validationEmail">
                 <Form.Control
                     type="email"
                     name="email"
+                    placeholder="Email"
                     value={formik.values.email}
                     onChange={formik.handleChange}
                     onBlur={formik.handleBlur}
-                    isValid={
-                        formik.touched && formik.dirty && !formik.errors.email
+                    isInvalid={
+                        formik.submitCount && Boolean(formik.errors.email)
                     }
+                    required
                 />
-                <Form.Control.Feedback>Looks good</Form.Control.Feedback>
+                <Form.Control.Feedback type="invalid">
+                    {formik.errors.email}
+                </Form.Control.Feedback>
             </Form.Group>
             <Form.Group controlId="validationPassword">
                 <Form.Control
                     type="password"
                     name="password"
+                    placeholder="Password"
                     value={formik.values.password}
                     onChange={formik.handleChange}
                     onBlur={formik.handleBlur}
-                    isValid={
-                        formik.touched &&
-                        formik.dirty &&
-                        !formik.errors.password
+                    isInvalid={
+                        formik.submitCount && Boolean(formik.errors.password)
                     }
+                    required
                 />
-                <Form.Control.Feedback>Looks good</Form.Control.Feedback>
+                <Form.Control.Feedback type="invalid">
+                    {formik.errors.password}
+                </Form.Control.Feedback>
             </Form.Group>
             <Button variant="primary" type="submit">
                 Sign In
