d76b94b466a7057b10ae0b15b3b800777fce43a9
codemile <nick@reactgular.com>
2021-02-23 18:08:47 -0500

    updates login form

--- src/components/auth/AuthLoginForm/AuthLoginForm.tsx
+++ src/components/auth/AuthLoginForm/AuthLoginForm.tsx
@@ -1,8 +1,9 @@
+import {Button, TextField} from '@material-ui/core';
 import {Auth} from 'aws-amplify';
 import {useFormik} from 'formik';
 import {FunctionComponent, useState} from 'react';
-import {Button, Form} from 'react-bootstrap';
 import * as yup from 'yup';
+import {usePromise} from '../../../hooks/utils/usePromise';
 
 const schema = yup.object().shape({
     email: yup.string().email().required('Email is required.'),
@@ -10,76 +11,68 @@ const schema = yup.object().shape({
 });
 
 export const AuthLoginForm: FunctionComponent = () => {
-    const [invalidSignIn, setInvalidSignIn] = useState(false);
+    const [disabled, setDisabled] = useState(false);
+
+    const submit = usePromise(async ({email: username, password}) => {
+        try {
+            setDisabled(true);
+            const user = await Auth.signIn({username, password});
+        } finally {
+            setDisabled(false);
+        }
+    });
 
     const formik = useFormik({
         validationSchema: schema,
         initialValues: {email: '', password: ''},
-        onSubmit: async ({email: username, password}, formikHelpers) => {
-            try {
-                const user = await Auth.signIn({username, password});
-                console.log(user);
-            } catch (err) {
-                console.error(err);
-                if (err.name === 'UserNotFoundException') {
-                    setInvalidSignIn(true);
-                } else {
-                    throw err;
-                }
-            }
-        }
+        onSubmit: submit
     });
 
     return (
-        <Form
+        <form
             className="flex flex-col mb-6"
             noValidate
-            validated={Boolean(formik.submitCount)}
             onSubmit={formik.handleSubmit}
         >
-            <Form.Group controlId="validationEmail">
-                <Form.Control
-                    type="email"
-                    name="email"
-                    placeholder="Email"
-                    value={formik.values.email}
-                    onChange={formik.handleChange}
-                    onBlur={formik.handleBlur}
-                    isInvalid={
-                        formik.submitCount && Boolean(formik.errors.email)
-                    }
-                    required
-                />
-                <Form.Control.Feedback type="invalid">
-                    {formik.errors.email}
-                </Form.Control.Feedback>
-            </Form.Group>
-            <Form.Group controlId="validationPassword">
-                <Form.Control
-                    type="password"
-                    name="password"
-                    placeholder="Password"
-                    value={formik.values.password}
-                    onChange={formik.handleChange}
-                    onBlur={formik.handleBlur}
-                    isInvalid={
-                        formik.submitCount && Boolean(formik.errors.password)
-                    }
-                    required
-                />
-                <Form.Control.Feedback type="invalid">
-                    {formik.errors.password}
-                </Form.Control.Feedback>
-            </Form.Group>
-            {invalidSignIn && (
-                <div className="text-danger mb-4">
-                    The email or password you entered is incorrect. Please try
-                    again.
-                </div>
-            )}
-            <Button variant="primary" type="submit">
+            <TextField
+                fullWidth
+                name="email"
+                label="Email"
+                variant="outlined"
+                size="small"
+                disabled={disabled}
+                value={formik.values.email}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                error={formik.submitCount && Boolean(formik.errors.email)}
+                helperText={formik.errors.email || ' '}
+                required
+            />
+            <TextField
+                fullWidth
+                className="mb-1"
+                type="password"
+                name="password"
+                label="Password"
+                variant="outlined"
+                size="small"
+                disabled={disabled}
+                value={formik.values.password}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                error={formik.submitCount && Boolean(formik.errors.password)}
+                helperText={formik.errors.password || ' '}
+                required
+            />
+            <Button
+                variant="contained"
+                color="primary"
+                type="submit"
+                disableElevation
+                disabled={disabled}
+            >
                 Sign In
             </Button>
-        </Form>
+        </form>
     );
 };
